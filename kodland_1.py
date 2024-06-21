c951a5c4ecb92f0101ca73be393740e61b113a8b
FurkanSalihHayir <yusufziyahayir@hotmail.com>
2024-06-21 20:34:21 +0300

    kodland_1.py

---
+++ kodland_1.py
@@ -0,0 +1,7 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bulduğunuz herhangi bir şey için kullanılan kısaltma. (Laughing out loud)",
+            }
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
